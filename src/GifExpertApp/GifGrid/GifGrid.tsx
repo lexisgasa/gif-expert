22335ec541b81d8e15945af7535b50e42f8e53cc
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-10-14 17:30:51 +0200

    Create GifGrid component

---
+++ src/GifExpertApp/GifGrid/GifGrid.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import fetchGifs from "../../helpers/fetchGifs";
+import { GiphyGif } from "../../types";
+
+interface GifGridProps {
+  category: string;
+}
+
+const GifGrid = ({ category }: GifGridProps): React.ReactElement => {
+  const [gifs, setGifs] = useState<GiphyGif[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const fetchedGifs = await fetchGifs(category);
+      setGifs(fetchedGifs);
+    })();
+  }, [category]);
+
+  return (
+    <>
+      <ul>
+        <li>
+          <h3>{category} </h3>
+          <ul>
+            {gifs &&
+              gifs.map((gif) => (
+                <li key={gif.id}>
+                  <img
+                    src={gif.url}
+                    height={gif.height}
+                    width={gif.width}
+                    alt={gif.title}
+                  />
+                </li>
+              ))}
+          </ul>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default GifGrid;
